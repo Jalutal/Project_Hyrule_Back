b2d6a1643fb521dff29c62d278abd76ce92019a9
Jesse Granier <jesse.granier@lapiscine.pro>
2024-01-20 00:33:47 +0100

    WIP, near end

--- app.js
+++ app.js
@@ -5,42 +5,23 @@ const app = express()
 const port = 3000
 const cors = require('cors');
 
-
+// Configuration des packages
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-// Définir la route pour récupérer les fictions d'un utilisateur
-app.get('/user/:id/checkfictions', async (req, res) => {
-  const idUtilisateur = req.params.idUtilisateur;
-  try {
-    const fictions = await User.findAll({
-      where: { id: idUtilisateur },
-      include: [{ model: Fiction }],
-    });
-    res.json(fictions);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erreur lors de la récupération des fictions' });
-  }
-});
-
+// Configuration des routes
 const userRouter = require('./routes/userRoute')
 const fanficsRouter = require('./routes/fanficRoute')
 const commentsRouter = require('./routes/commentsRoute')
 
+// Configuration des routes
 app.use('/api/users', userRouter)
 app.use('/api/fanfics', fanficsRouter)
 app.use('/api/comments', commentsRouter)
 
-
-
-
-
+// Démarrage du serveur Express
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
